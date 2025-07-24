5ae566b5efb4fb7d23c567c668b533ca4bf471fb
Gokul P <gokulgokul638244g@gmail.com>
2025-07-24 23:52:41 +0530

    Create ProgramwithRandomNum&Arr1.java

---
+++ ProgramwithRandomNum&Arr1.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.ThreadLocalRandom;
+public class Main {
+    public static void main(String[] args) {
+        int[] arr=new int[10];
+        for(int i=0;i<10;i++){
+            int val= ThreadLocalRandom.current().nextInt(0,1000);
+            arr[i]=val;
+        }
+        for(int i=0;i<10;i++)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+        for(int i=0;i<5;i++){
+            int rand=ThreadLocalRandom.current().nextInt(0,arr.length);
+            int[] tempArr=new int[arr.length-1];
+            int idx=0;
+            for(int j=0;j<arr.length;j++){
+                if(rand==j) continue;
+                tempArr[idx++]=arr[j];
+            }
+            arr=tempArr;
+        }
+        for(int i=0;i<arr.length;i++)
+            System.out.print(arr[i]+" ");
+    }
+
+}
+
